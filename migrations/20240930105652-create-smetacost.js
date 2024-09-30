6d81af0e382d08d8e60335fc43975c51d3bd0752
nuruddin <sg7720@gmail.com>
2024-09-30 18:37:01 +0300

    изменения в уч записях в индекс моделс отмена синхронизации секуалайз

---
+++ migrations/20240930105652-create-smetacost.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Smetacosts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      reason: {
+        type: Sequelize.STRING
+      },
+      sum: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Smetacosts');
+  }
+};
