cbb33cfd22ef49bdb6a3486da126f282f82798da
nuruddin <sg7720@gmail.com>
2024-11-16 12:54:08 +0300

    add origin

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    // origin: 'http://188.68.220.210:3000'
-    origin: 'http://localhost:3000'
+    origin: ['http://188.68.220.210:3000', 'http://188.68.220.210:12345']
 }));
 app.use(express.static(root))
 app.use('/', router);
