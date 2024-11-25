bbccf9cf6e5c6c42ce791acbcc2391e11ab5e8b2
nuruddin <sg7720@gmail.com>
2024-11-25 17:37:40 +0300

    no control auth

--- router/index.js
+++ router/index.js
@@ -41,7 +41,7 @@ router.post("/docspec", authMiddleware, specialityController.create);
 router.get("/docspecs", authMiddleware, specialityController.getAll);
 router.get('/docspecs/:id', authMiddleware, specialityController.deleteSpeciality)
 router.get('/smetas', authMiddleware, smetaController.getAll);
-router.get('/smetas/:id', authMiddleware, smetaController.getOne);
+router.get('/smetas/:id', smetaController.getOne);
 router.post('/smetas-mkrd', authMiddleware, smetaController.updateSmeta);
-router.post('/smetas-upd-f', smetaController.updateSmetaFields);
+router.post('/smetas-upd-f', authMiddleware, smetaController.updateSmetaFields);
 module.exports = router
