cf2649524c534236471d48f808576978d0862945
nuruddin <sg7720@gmail.com>
2024-11-28 17:53:23 +0300

    изменил файл миграции, сделал добавление колонки вместо изменения колонки

--- migrations/20241128143528-add-col-smeta.js
+++ migrations/20241128143528-add-col-smeta.js
@@ -1,17 +1,18 @@
 'use strict';
 
 module.exports = {
-    up: async (queryInterface, Sequelize) => {
-        await queryInterface.changeColumn('Smeta', 'totalAllSum', {
-            type: Sequelize.STRING,
-            allowNull: true,
-        });
+    async up(queryInterface, Sequelize) {
+        return Promise.all([
+            queryInterface.addColumn('Smeta', 'totalAllSum', {
+                type: Sequelize.DataTypes.STRING,
+                allowNull: true,
+            }),
+        ]);
     },
 
-    down: async (queryInterface, Sequelize) => {
-        await queryInterface.changeColumn('Smeta', 'totalAllSum', {
-            type: Sequelize.STRING,
-            allowNull: true,
-        });
+    async down(queryInterface) {
+        return Promise.all([
+            queryInterface.removeColumn('Smeta', 'totalAllSum' ),
+        ]);
     }
 };
