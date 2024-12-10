d3d307d64d78775f0403835c2f686bd141e2fba3
nuruddin <sg7720@gmail.com>
2024-12-10 18:22:14 +0300

    миграция добавление валюты во многие таблицы

---
+++ migrations/20241210151033-add-currency-to-tables.js
@@ -0,0 +1,66 @@
+'use strict';
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return Promise.all([
+            queryInterface.addColumn(
+                'Smetaplans',
+                'currency',
+                {
+                    type: Sequelize.STRING,
+                    allowNull: true,
+                }
+            ),
+            queryInterface.addColumn(
+                'Smetaroadcosts',
+                'currency',
+                {
+                    type: Sequelize.STRING,
+                    allowNull: true,
+                }
+            ),
+            queryInterface.addColumn(
+                'Smetaroaccomodations',
+                'currency',
+                {
+                    type: Sequelize.STRING,
+                    allowNull: true,
+                }
+            ),
+            queryInterface.addColumn(
+                'Smetamealcosts',
+                'currency',
+                {
+                    type: Sequelize.STRING,
+                    allowNull: true,
+                }
+            ),
+            queryInterface.addColumn(
+                'Smetatransportcosts',
+                'currency',
+                {
+                    type: Sequelize.STRING,
+                    allowNull: true,
+                }
+            ),
+            queryInterface.addColumn(
+                'Smetacosts',
+                'currency',
+                {
+                    type: Sequelize.STRING,
+                    allowNull: true,
+                }
+            ),
+        ]);
+    },
+    down: (queryInterface, Sequelize) => {
+        return Promise.all([
+            queryInterface.removeColumn('Smetaplans', 'currency'),
+            queryInterface.removeColumn('Smetaroadcosts', 'currency'),
+            queryInterface.removeColumn('Smetaroaccomodations', 'currency'),
+            queryInterface.removeColumn('Smetamealcosts', 'currency'),
+            queryInterface.removeColumn('Smetatransportcosts', 'currency'),
+            queryInterface.removeColumn('Smetacosts', 'currency'),
+        ]);
+    }
+};
