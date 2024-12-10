056d884bccba7422bb9fc9a772234c495dae0705
nuruddin <sg7720@gmail.com>
2024-12-10 18:53:06 +0300

    миграция на удаление колонки

---
+++ migrations/20241210155159-remove-col-medicine.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        // Remove the column
+        await queryInterface.removeColumn('Smetaplans', 'medicine');
+    },
+
+    down: async (queryInterface, Sequelize) => {
+        // Add the column back (if rollback is needed)
+        await queryInterface.addColumn('Smetaplans', 'medicine', {
+            type: Sequelize.STRING, // Use the appropriate type
+            allowNull: true,        // Use the correct constraints
+        });
+    },
+};
