41303f46c9ac87a3a080f72a2bf18ed7d5c72910
nuruddin <sg7720@gmail.com>
2025-05-15 21:20:11 +0300

    get user sign

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: ['http://188.68.220.210:3000', 'http://188.68.220.210:12345','http://localhost:3000'],
+    origin: ['http://188.68.220.210:3000', 'http://188.68.220.210:12345','http://localhost:3000','http://localhost:9000'],
 }));
 app.use(express.static(root))
 app.use('/', router);
