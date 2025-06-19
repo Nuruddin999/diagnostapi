4c6e734da9c733b0dc3ae00e2a8d9bd4cac4621c
nuruddin <sg7720@gmail.com>
2025-06-19 03:22:30 +0300

    users analytics

--- controllers/analytics-controller.js
+++ controllers/analytics-controller.js
@@ -1,13 +1,18 @@
 const {User, Application} = require('../models');
 const {Op} = require('sequelize');
 const dayjs = require("dayjs");
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
 
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
-const now = dayjs().toDate();
-const weekAgo =dayjs().utc().subtract(1, 'week').toDate();
-const monthAgo = dayjs().utc().subtract(1, 'month').toDate();
-const dayAgoStart = dayjs().utc().subtract(1, 'days').startOf('day').toDate()
-const dayAgoEnd = dayjs().utc().subtract(1, 'days').endOf('day').toDate()
+const MSK = 'Europe/Moscow';
+const now = dayjs().tz(MSK).toDate();
+const weekAgo = dayjs().tz(MSK).subtract(1, 'week').toDate();
+const monthAgo = dayjs().tz(MSK).subtract(1, 'month').toDate();
+const dayAgoStart = dayjs().tz(MSK).subtract(1, 'day').startOf('day').toDate();
+const dayAgoEnd = dayjs().tz(MSK).subtract(1, 'day').endOf('day').toDate();
  console.log('weekAgo',weekAgo);
 console.log('monthAgo',monthAgo);
 console.log('dayAgoStart',dayAgoStart);
@@ -16,25 +21,31 @@ console.log('dayAgoEnd',dayAgoEnd);
 const periodMap = {
     week:{[Op.between]:[weekAgo, now]},
     month:{[Op.between]:[monthAgo, now]},
-    today:{[Op.between]:[dayjs().startOf('day').toDate(),now]},
+    today:{[Op.between]: [dayjs().tz(MSK).startOf('day').toDate(), now]},
     yesterday:{[Op.between]:[dayAgoStart,dayAgoEnd]},
  }
 
 class AnalyticsController {
     async getUsersRecap(req,res,next){
-        const {period} = req.query;
-        console.log('period',period);
-        console.log('map',periodMap[period]);
-        const users = await User.findAll();
-        const applications = await Application.findAll({
-           where: {
-               managerId:{[Op.in]:users.map(el=>el.id.toString())},
-               createdAt:periodMap[period],
-               passToCoordinatorTime:periodMap[period],
-           }
-       });
-        const processedUsers = [...users].map(el=>({name: el.dataValues.name, speciality: el.dataValues.speciality ,applications: applications.filter(appl=>appl.managerId.toString() === el.dataValues.id.toString()).length}));
-        return res.json({users: processedUsers});
+        try {
+            const {period} = req.query;
+            console.log('period',period);
+            console.log('map',periodMap[period]);
+            const users = await User.findAll();
+            const applications = await Application.findAll({
+                where: {
+                    managerId:{[Op.in]:users.map(el=>el.id.toString())},
+                    createdAt:periodMap[period],
+                    passToCoordinatorTime:periodMap[period],
+                }
+            });
+            const processedUsers = [...users].map(el=>({name: el.dataValues.name, speciality: el.dataValues.speciality ,applications: applications.filter(appl=>appl.managerId.toString() === el.dataValues.id.toString()).length}));
+            return res.json({users: processedUsers});
+        }
+        catch(err){
+            next(err);
+        }
+
     }
 }
 
