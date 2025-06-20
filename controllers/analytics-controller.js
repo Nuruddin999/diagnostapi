03fe3db2200b9e406a79d0fb0201050e960e3546
nuruddin <sg7720@gmail.com>
2025-06-20 20:24:17 +0300

    change manager

--- controllers/analytics-controller.js
+++ controllers/analytics-controller.js
@@ -37,7 +37,7 @@ class AnalyticsController {
                 }
             });
             const processedUsers = [...users].map(el=>({name: el.dataValues.name, speciality: el.dataValues.speciality ,applications: applications.filter(appl=>appl.managerId.toString() === el.dataValues.id.toString()).length}));
-            return res.json({users: processedUsers});
+            return res.json({users: processedUsers, count: applications.length});
         }
         catch(err){
             next(err);
