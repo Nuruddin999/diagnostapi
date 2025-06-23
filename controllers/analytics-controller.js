0cfe43da8090b39a5df383ee2982278f85341fec
nuruddin <sg7720@gmail.com>
2025-06-23 20:39:33 +0300

    correct user item

--- controllers/analytics-controller.js
+++ controllers/analytics-controller.js
@@ -69,7 +69,7 @@ class AnalyticsController {
                 applications: applications.map(appl => ({name:appl.dataValues.patientName, birth: appl.dataValues.patientBirthDate, createdAt:appl.dataValues.createdAt, passToCoordinatorTime:appl.dataValues.passToCoordinatorTime}))
             }
 
-            console.log('periods',periodMap[period][[Op.between]])
+            console.log('periods',periodMap[period][Op.between])
 
             return res.json({user: processedUser, count: applications.length, period: ''});
         } catch (err) {
