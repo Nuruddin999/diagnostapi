9d56b0f5044d9baba712f27c914bcea7b956bcfd
nuruddin <sg7720@gmail.com>
2025-06-24 22:35:56 +0300

    correct user application duration

--- controllers/analytics-controller.js
+++ controllers/analytics-controller.js
@@ -30,8 +30,8 @@ class AnalyticsController {
         try {
             const {period} = req.query;
             const users = await User.findAll();
-            console.log('user[]0',users[0]);
-            console.log('mapped',users.map(el => el.id.toString()))
+            console.log('user[]0',users[0].dataValues);
+            console.log('mapped',users.map(el => el.dataValues.id.toString()))
             const applications = await Application.findAll({
                 where: {
                     managerId: {[Op.in]: users.map(el => el.id.toString())},
