6ea4981f02c3752cc51f583676bf5893e0201755
nuruddin <sg7720@gmail.com>
2025-10-15 18:21:27 +0300

    conditional status

---
+++ middlewares/donationa-auth-middleware.js
@@ -0,0 +1,14 @@
+const ApiError = require('../exceptions/api-error');
+const getUserData = require("./userData");
+
+module.exports = function (req, res, next) {
+    try {
+        req.user = getUserData(req, res, next);
+        if (req.user.role !== 'charity') {
+            return next(ApiError.UnauthorizedError());
+        }
+        next();
+    } catch (e) {
+        return next(ApiError.UnauthorizedError());
+    }
+};
